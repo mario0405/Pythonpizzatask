b77bdb6a653095518fb61f97fd7cc342b0b48458
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-09-25 14:41:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from x import y
+
+def main():
+    while True:
+        print("\nPizza Menu:")
+        print("1. List all pizzas")
+        print("2. Find pizzas with a specific topping")
+        print("3. Select a pizza by name")
+        print("4. Calculate pizza price with discount code")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+  
