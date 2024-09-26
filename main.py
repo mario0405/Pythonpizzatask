009465c3efd7743e905695a9ff9e522c0cb8854d
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-09-26 16:06:05 +0200

    Update main.py
    if statements grob erstellt

--- main.py
+++ main.py
@@ -10,4 +10,17 @@ def main():
         print("5. Exit")
         choice = input("Enter your choice: ")
 
-  
+        if choice == "1": #Alle Pizzas auflisten
+            xy
+
+        elif choice == "2": #Pizzen mit xy Topping
+            xy
+        
+        elif choice == "3": #Pizzen mit xy Namen
+            xy
+
+        elif choice == "4": #Preis mit Rabatt berechnen
+            xy
+
+        elif choice == "5": #Programm verlassen
+            xy
