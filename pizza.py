3032c6b2e2ab28297d8e94547fd5259fd3cc37b9
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-09-26 16:09:45 +0200

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,8 +1,12 @@
 class Pizza:
-  def __init__(self, .....):
-
+  def __init__(self, name, toppings, price):
+    self.name = name
+    self.toppings = toppings
+    self.price = price
 
   def get_details(self):
+  
 
 
-  def has_topping(self, ):
+  def has_topping(self, topping):
+    return topping in self.toppings
