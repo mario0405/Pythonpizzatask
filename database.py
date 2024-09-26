659b1895c47e6edc56f0ea767d3f9012ee09a1d5
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-09-26 15:58:28 +0200

    Update database.py
    Liste in einer Liste, also nicht eine grosse Liste

--- database.py
+++ database.py
@@ -1,3 +1,6 @@
-pizzas = [Pizza("Sorte", ("Zutaten"), "Preis"),    #Liste von allen Pizzen inkl Zutaten und Preis
-         Pizza("Sorte", etc.)]
-
+pizzas = pizzas = [                  #Liste von allen Pizzen inkl Zutaten und Preis l Eine Liste in einer Liste
+    Pizza("Margherita", ["tomato", "mozzarella"], 8.50),
+    Pizza("Pepperoni", ["tomato", "mozzarella", "pepperoni"], 9.50),
+    Pizza("Hawaiian", ["ham", "pineapple"], 10.00),
+    Pizza("Veggie", ["tomato", "mozzarella", "peppers", "onions"], 9.00)
+]    
