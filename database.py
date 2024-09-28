697b34741251c2757ec2c42efc840dfa49e2d02b
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-09-28 12:34:26 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,15 @@
-pizzas = pizzas = [                  #Liste von allen Pizzen inkl Zutaten und Preis l Eine Liste in einer Liste
+from pizza import Pizza
+
+pizzas = [                                   #4 Bsp Pizzen mit deren Toppings und Preis l alles in einer sogenannten mock database
     Pizza("Margherita", ["tomato", "mozzarella"], 8.50),
     Pizza("Pepperoni", ["tomato", "mozzarella", "pepperoni"], 9.50),
     Pizza("Hawaiian", ["ham", "pineapple"], 10.00),
     Pizza("Veggie", ["tomato", "mozzarella", "peppers", "onions"], 9.00)
-]    
+]
+
+def list_all_pizzas():
+
+def find_pizzas_with_topping():
+
+def find_pizza_by_name():
+    for pizza in pizzas:
