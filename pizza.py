0142a66986dc86f5f7c40e16e7453a8fc66d8e44
mario0405 <161290834+mario0405@users.noreply.github.com>
2024-10-19 17:08:37 +0200

    Kommentäre hinzugefügt

--- pizza.py
+++ pizza.py
@@ -1,12 +1,15 @@
 class Pizza:
+    # Defines the Pizza class
     def __init__(self, name, toppings, price):
+        # Initialize pizza with a name, toppings, and price
         self.name = name
         self.toppings = toppings
         self.price = price
 
     def get_details(self):
+        # Returns the pizza's details as a string (name, toppings, and price)
         return self.name + ": " + ", ".join(self.toppings) + " - $" + str(round(self.price, 2))
-        # noch vereinfachen...
 
     def has_topping(self, topping):
+        # Checks if the pizza has the specified topping
         return topping in self.toppings
